49d6dba2b8565c71815a9d21a3aabac3a4fe93c8
Michael Ofagbor <eihmenormike@gmail.com>
2024-06-25 10:05:18 -0700

    deploying assets

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let slides = document.querySelectorAll('.reviews .flex .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+let monthBtn = document.querySelector('.pricing .toggle-buttons .month-btn');
+let yearBtn = document.querySelector('.pricing .toggle-buttons .year-btn');
+let monthlyPlan = document.querySelectorAll('.pricing .box-container .box .month');
+let yearlyPlan = document.querySelectorAll('.pricing .box-container .box .year');
+
+yearBtn.onclick = () =>{
+    monthBtn.classList.remove('active');
+    yearBtnBtn.classList.add('active');
+
+    monthlyPlan.forEach(mo => {mo.style.display = 'none'});
+    yearlyPlan.forEach(yr => {yr.style.display = 'block'});
+}
+
+monthBtn.onclick = () =>{
+    monthBtn.classList.add('active');
+    yearBtnBtn.classList.remove('active');
+
+    monthlyPlan.forEach(mo => {mo.style.display = 'block'});
+    yearlyPlan.forEach(yr => {yr.style.display = 'none'});
+}
